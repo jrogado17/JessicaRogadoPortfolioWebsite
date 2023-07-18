948ac33b96c27b90740a952543ba2a924a3b2184
Jessica Rogado <j.rogado17@yahoo.com>
2023-07-18 13:12:50 -0700

    nav bar hire me popup, img, and Jessica Rogado rendering'

--- my-app/src/features/Navbar/navbar.js
+++ my-app/src/features/Navbar/navbar.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './navbar.css';
+import hireImg from '../images/hireMe.png';
 
 const Navbar = () => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleTogglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  };
+
   return (
     <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+        <div id='name'>
+            <p>Jessica Rogado</p>
+        </div>
+      <div className="hire-container" onMouseEnter={handleTogglePopup} onMouseLeave={handleTogglePopup}>
+      <img src={hireImg} alt="Hire Me" className="hire-image" />
+        <p className="hire-me">HIRE ME</p>
+        {isPopupVisible && (
+          <div className="popup">
+            <p>
+              Let's connect and explore how I can bring my technical knowledge and hands-on experience to your next innovative project.
+            </p>
+          </div>
+        )}
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App/App';
 
-ReactDOM.render(
+render(
   <Router>
     <App />
   </Router>,
