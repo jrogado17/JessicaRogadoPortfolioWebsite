ee53c4a4feeca0d98dee41744a9b7b842310e379
Jessica Rogado <j.rogado17@yahoo.com>
2023-09-08 15:35:44 -0700

    css process

--- my-app/src/features/LandingPage/Sections/welcome.js
+++ my-app/src/features/LandingPage/Sections/welcome.js
@@ -3,9 +3,9 @@ import welcomeImage from '../../images/Welcome.png';
 
 const Welcome = () => {
   return (
-    <section id="about-me" className="section">
+    <section id="welcome" className="welcomeSection">
       <img src={welcomeImage} alt="Welcome" /> 
-      <p>
+      <p id='welcomeText'>
         I'm Jessica Rogado, a software engineer rooted in physics and mechanical engineering, now diving into full-stack development to fuel my tech-driven problem-solving passion.
       </p>
       <p id='lb'>Based in Long Beach, CA</p>
