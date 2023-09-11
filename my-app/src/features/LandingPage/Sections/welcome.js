ff8a0493e048bf3e9f40e3602c8e38464d5d5979
Jessica Rogado <j.rogado17@yahoo.com>
2023-09-11 16:40:25 -0700

    add text styling and align content to the center

--- my-app/src/features/LandingPage/Sections/welcome.js
+++ my-app/src/features/LandingPage/Sections/welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import welcomeImage from '../../images/Welcome.png'; 
+import welcomeImage from '../../images/Welcome.png';
+import './welcome.css'
 
 const Welcome = () => {
   return (
-    <section id="welcome" className="welcomeSection">
-      <img src={welcomeImage} alt="Welcome" /> 
+    <section className="welcomeSection">
+      <img src={welcomeImage} id="welcome" alt="Welcome" /> 
       <p id='welcomeText'>
         I'm Jessica Rogado, a software engineer rooted in physics and mechanical engineering, now diving into full-stack development to fuel my tech-driven problem-solving passion.
       </p>
