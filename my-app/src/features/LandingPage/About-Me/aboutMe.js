7e161d2d48697f9f606a1b873564a62e9e7049c4
Jessica Rogado <j.rogado17@yahoo.com>
2023-09-14 11:21:28 -0700

    picture added

--- my-app/src/features/LandingPage/About-Me/aboutMe.js
+++ my-app/src/features/LandingPage/About-Me/aboutMe.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import picture from '../../images/aboutmepic.png'
 
 const AboutMe = () => {
-  return (
-    <div className="about-me">
-      <h2>Introduction</h2>
-      <p>
-        I'm Jess, a Software Engineer with a passion for creating innovative software solutions.
-        I specialize in full-stack development with an interest in machine learning and UX/UI.
-        I'm dedicated to solving complex problems through clean code and efficient software design.
-        I believe in staying up-to-date with industry best practices. This approach allows me to deliver
-        high-quality software that not only meets but exceeds expectations.
-      </p>
-    </div>
-  );
-};
-
-export default AboutMe;
+    return (
+      <div className="about-me">
+        <div className="picture">
+          <img src={picture} alt="About Me" />
+        </div>
+        <div className="text">
+          <h2>Introduction</h2>
+          <p>
+            I'm Jess, a Software Engineer with a passion for creating innovative software solutions.
+            I specialize in full-stack development with an interest in machine learning and UX/UI.
+            I'm dedicated to solving complex problems through clean code and efficient software design.
+            I believe in staying up-to-date with industry best practices. This approach allows me to deliver
+            high-quality software that not only meets but exceeds expectations.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AboutMe;
