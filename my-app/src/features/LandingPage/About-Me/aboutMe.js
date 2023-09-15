eb6da82b3b52f9445151501131137dc5aa68d195
Jessica Rogado <j.rogado17@yahoo.com>
2023-09-14 17:10:45 -0700

    about me header aligned

--- my-app/src/features/LandingPage/About-Me/aboutMe.js
+++ my-app/src/features/LandingPage/About-Me/aboutMe.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import picture from '../../images/aboutmepic.png'
+import header from '../../images/aboutMeHeader.png'
 import './aboutMe.css'
 
 const AboutMe = () => {
     return (
-      <div className="about-me">
-        <div className="pictureofMe">
-          <img src={picture} alt="About Me" />
+      <div className = 'aboutmeContainer'>
+        <div className = 'aboutmeHeader'>
+            <img src={header} alt="About Me Header" />
         </div>
-        <div className="about-me-text">
-          <h2 id='Introduction'>Introduction</h2>
-          <p>
-            I'm Jess, a Software Engineer with a passion for creating innovative software solutions.
-            I specialize in full-stack development with an interest in machine learning and UX/UI.
-            I'm dedicated to solving complex problems through clean code and efficient software design.
-            I believe in staying up-to-date with industry best practices. This approach allows me to deliver
-            high-quality software that not only meets but exceeds expectations.
-          </p>
+        <div className="about-me">
+            <div className="pictureofMe">
+                <img src={picture} alt="Picture of Me" />
+            </div>
+            <div className="about-me-text">
+                <h2 id='Introduction'>Introduction</h2>
+                <p>
+                 I'm Jess, a Software Engineer with a passion for creating innovative software solutions.
+                 I specialize in full-stack development with an interest in machine learning and UX/UI.
+                    I'm dedicated to solving complex problems through clean code and efficient software design.
+                    I believe in staying up-to-date with industry best practices. This approach allows me to deliver
+                    high-quality software that not only meets but exceeds expectations.
+                </p>
+            </div>
         </div>
       </div>
     );
