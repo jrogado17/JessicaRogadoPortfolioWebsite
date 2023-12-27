2a32caeacae6d5db19c80536316465a0d0c817c8
Jessica Rogado <j.rogado17@yahoo.com>
2023-12-27 13:59:45 -0800

    redesigning welcome message

--- my-app/src/features/LandingPage/Welcome/welcome.js
+++ my-app/src/features/LandingPage/Welcome/welcome.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import welcomeImage from '../../images/Welcome.png';
-import './welcome.css'
+import './welcome.css';
 
 const Welcome = () => {
   return (
-    <div className="welcomeSection">
-      <img src={welcomeImage} id="welcome" alt="Welcome" /> 
-      <p id='welcomeText'>
-        I'm Jessica Rogado, a software engineer rooted in physics and mechanical engineering, now diving into full-stack development to fuel my tech-driven problem-solving passion.
-      </p>
-      <p id='lb'>Based in Long Beach, CA</p>
+    <div className="welcomeSection" id='welcomeText'>
+      <div>
+        <h1>I'm <span className="italic-text">Jessica Rogado</span>,</h1>
+      </div>
+      <div>
+        <h1>a <span className="italic-text">Mechanical Engineer</span></h1>
+      </div>
+      <div>
+        <h1>with a background of <span className="italic-text">Physics</span></h1>
+      </div>
+      <div>
+        <h1>based in <span className="italic-text">Long Beach, CA</span></h1>
+      </div>
+      <div>
+        <h1>specializing in <span className="italic-text">Software Development</span></h1>
+      </div>
     </div>
   );
 };
