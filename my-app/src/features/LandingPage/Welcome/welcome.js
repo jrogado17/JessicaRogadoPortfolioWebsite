9416448d869542ba96e8b55b8bcb59357ab527b1
Jessica Rogado <j.rogado17@yahoo.com>
2023-12-29 10:29:03 -0800

    navbar redesign

--- my-app/src/features/LandingPage/Welcome/welcome.js
+++ my-app/src/features/LandingPage/Welcome/welcome.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './welcome.css';
 
+
 const Welcome = () => {
   return (
-    <div className="welcomeSection" id='welcomeText'>
-      <div>
-        <h1>I'm <span className="italic-text">Jessica Rogado</span>,</h1>
+    <div className="welcomeContainer" id='welcomeText'>
+      <div id='line' class="display-text-div">
+        <h1 class="display-heading">Hi, I'm <span className="italic-text">Jessica Rogado</span></h1>
+        <img src="https://uploads-ssl.webflow.com/62256e3694580b7ffbcea5c7/62282ae713de7f7a5a22cf77_flower-symbol.svg" 
+        loading="lazy" alt="flower"class="flower-img"></img>
       </div>
-      <div>
+      <div id='line'>
         <h1>a <span className="italic-text">Mechanical Engineer</span></h1>
       </div>
-      <div>
+      <div id='line'>
         <h1>with a background of <span className="italic-text">Physics</span></h1>
       </div>
-      <div>
+      <div id='line'>
         <h1>based in <span className="italic-text">Long Beach, CA</span></h1>
       </div>
-      <div>
+      <div id='line'>
         <h1>specializing in <span className="italic-text">Software Development</span></h1>
       </div>
     </div>
--- my-app/src/features/Navbar/navbar.js
+++ my-app/src/features/Navbar/navbar.js
@@ -1,25 +1,15 @@
-import React from 'react';
+import * as React from "react";
+import { Link} from "react-router-dom";
 import './navbar.css';
-import linkedinIcon from '../images/linkedinIcon.png'; 
-import githubIcon from '../images/githubIcon.png'; 
-import emailIcon from '../images/emailIcon.png'; 
+
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='nameTitle'>
-        <p id='name'>Jessica Rogado</p>
-      </div>
-      <div className="social-links">
-        <a href="https://github.com/your-github-link" target="_blank" rel="noopener noreferrer">
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
-          <img src={linkedinIcon} alt="LinkedIn"/>
-        </a>
-        <a href="mailto:jrogado23@yahoo.com">
-          <img src={emailIcon} alt="Email" />
-        </a>
+    <nav id='NavBar' role="banner">
+      <div className='navbarContainer'>
+        <Link to="/" id='name'>
+          JR
+        </Link>
       </div>
     </nav>
   );
