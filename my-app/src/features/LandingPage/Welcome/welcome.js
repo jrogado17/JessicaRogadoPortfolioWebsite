bcdb97923c686c6efe9acadb2afc07c510670b81
Jessica Rogado <j.rogado17@yahoo.com>
2023-12-29 11:38:07 -0800

    added style to the welcome text and added a loading effect

--- my-app/src/features/LandingPage/Welcome/welcome.js
+++ my-app/src/features/LandingPage/Welcome/welcome.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './welcome.css';
 
-
 const Welcome = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Wait for the page to fully load
+    window.addEventListener('load', () => {
+      // Add the 'loaded' class to trigger the slide-up effect
+      setLoaded(true);
+    });
+  }, []);
+
   return (
-    <div className="welcomeContainer" id='welcomeText'>
-      <div id='line' class="display-text-div">
-        <h1 class="display-heading">Hi, I'm <span className="italic-text">Jessica Rogado</span></h1>
-        <img src="https://uploads-ssl.webflow.com/62256e3694580b7ffbcea5c7/62282ae713de7f7a5a22cf77_flower-symbol.svg" 
-        loading="lazy" alt="flower"class="flower-img"></img>
+    <div className={`welcomeContainer ${loaded ? 'loaded' : ''}`} id='welcomeText'>
+      <div id='line1' className="display-text-div line slide-left" style={{animationDelay: '0.2s'}}>
+        <h1 className="display-heading">Hi, I'm <span className="italic-text">Jessica Rogado</span>
+          <img
+            src="https://uploads-ssl.webflow.com/62256e3694580b7ffbcea5c7/62282ae713de7f7a5a22cf77_flower-symbol.svg"
+            loading="lazy" alt="flower" className="flower-img">
+          </img>
+        </h1>
       </div>
-      <div id='line'>
-        <h1>a <span className="italic-text">Mechanical Engineer</span></h1>
+      <div id='line2' className="display-text-div line slide-right" style={{animationDelay: '0.4s'}}>
+        <h1 className="display-heading">a <span className="italic-text">Mechanical Engineer</span></h1>
       </div>
-      <div id='line'>
-        <h1>with a background of <span className="italic-text">Physics</span></h1>
+      <div id='line3' className="display-text-div line slide-left" style={{animationDelay: '0.6s'}}>
+        <h1 className="display-heading">with a background of <span className="italic-text">Physics</span></h1>
       </div>
-      <div id='line'>
-        <h1>based in <span className="italic-text">Long Beach, CA</span></h1>
+      <div id='line4' className="display-text-div line slide-right" style={{animationDelay: '0.8s'}}>
+        <h1 className="display-heading">based in <span className="italic-text">Long Beach, CA</span></h1>
       </div>
-      <div id='line'>
-        <h1>specializing in <span className="italic-text">Software Development</span></h1>
+      <div id='line5' className="display-text-div line slide-left" style={{animationDelay: '1.0s'}}>
+        <h1 className="display-heading">specializing in <span className="italic-text">Software Development</span></h1>
       </div>
     </div>
   );
