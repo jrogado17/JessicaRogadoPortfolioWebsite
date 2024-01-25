c9abd833e7f8b5208be987da1590452701042c1e
Jessica Rogado <j.rogado17@yahoo.com>
2024-01-24 18:07:41 -0800

    saving progress

--- my-app/src/features/LandingPage/About-Me/aboutMe.js
+++ my-app/src/features/LandingPage/About-Me/aboutMe.js
@@ -8,10 +8,10 @@ import Contact from '../Contact/contact'
 const AboutMe = () => {
   return (
     <div className='aboutmeContainer'>
-      <h2 className='PageTitle' id='Introduction'>
-        About Me
-      </h2>
-      <div className='introduction-section'>
+       <h2 className='PageTitle' id='Introduction'>
+            About Me
+          </h2>
+          <div className='introduction-section'>
         <h2 className='title' id='Introduction'>
           Introduction
         </h2>
@@ -21,80 +21,9 @@ const AboutMe = () => {
           I seamlessly integrated into the world of software, seizing the opportunity to earn a certification in software development. 💻✨
         </p>
       </div>
-      <div className='school-container'>
-        <h2 className='title' id='School'>
-          School
-        </h2>
-        <p className='school'>
-          I graduated{' '}
-          <span id='school-text'>
-            California State University, Long Beach
-            <img
-              src='https://digitalskills.cpace.csulb.edu/wp-content/uploads/cropped-cropped-favicon.png'
-              loading='lazy'
-              className='img'
-              alt='CSULB Logo'
-            />
-          </span>{' '}
-          with a{' '}
-          <span id='ME'>
-            <br />B.S. in Mechanical Engineering
-            <img
-              src='https://static.vecteezy.com/system/resources/thumbnails/016/415/298/small/wrench-illustration-in-minimal-style-png.png'
-              loading='lazy'
-              className='img'
-              alt='Mechanical Engineering Icon'
-            />
-          </span>{' '}
-          and a{' '}
-          <span id='ME'>
-            B.A. in Physics
-            <img
-              src='https://icons.veryicon.com/png/o/education-technology/educational-icon/physics-4.png'
-              loading='lazy'
-              className='img'
-              alt='Physics Icon'
-            />
-          </span>
-          <br />I also completed{' '}
-          <span>
-            Fullstack Academy{' '}
-            <img
-              src='https://s3.us-east-1.amazonaws.com/fsa2-assets/assets/Grace-Hopper/GH-Full-Lockup-Chalk.png'
-              loading='lazy'
-              className='gh-img'
-              alt='Fullstack Academy Icon'
-            />
-          </span>{' '}
-          for a certification in{' '}
-          <span id='SWE'>
-            <br />Software Development{' '}
-            <img
-              src='https://cdn-icons-png.flaticon.com/512/8759/8759045.png'
-              loading='lazy'
-              className='img'
-              alt='Software Development Icon'
-            />
-          </span>
-        </p>
-      </div>
-      <div className='skills-container'>
-        <h2 className='title' id='Skills'>
-          Skills
-        </h2>
-        <span id='skills'>
-          where I learned ...
-          <Skills />
-          I also know ...
-          <SchoolSkills/>
-        </span>
-      </div>
-      <div className='interests-container'>
-        <h2 className='title' id='Interests'>
-          Interests
-        </h2>
-        <div className='about-me-text'>
-          <p className='interests'>
+      <div className='interestsContainer'>
+        <div className='interests-text'>
+            <p className='interests'>
             I love to surf
             <img
               src='https://cdn-icons-png.flaticon.com/512/3145/3145024.png' 
@@ -115,23 +44,8 @@ const AboutMe = () => {
                 alt='Netherlands Flag'
               />
             Where I've been...
-            <ul className='country-list'>
-            <li>
-              <span>Netherlands</span>
-              <img
-                src='https://cdn-icons-png.flaticon.com/512/323/323275.png'
-                className='flag-img'
-                alt='Netherlands Flag'
-              />
-            </li>
-            <li>
-              <span>Denmark</span>
-              <img
-                src='https://cdn-icons-png.flaticon.com/512/197/197565.png'
-                className='flag-img'
-                alt='Denmark Flag'
-              />
-            </li>
+          </p>
+          <ul className='country-list'>
             <li>
               <span>Netherlands</span>
               <img
@@ -179,29 +93,90 @@ const AboutMe = () => {
                 className='flag-img'
                 alt='Norway Flag'
               />
-                </li>
-                <li>
-                  <span>Portugal</span>
-                  <img
-                    src='https://cdn-icons-png.flaticon.com/512/3909/3909361.png'
-                    className='flag-img'
-                    alt='Portugal Flag'
-                  />
-                </li>
-                <li>
-                  <span>Philippines</span>
-                  <img
-                    src='https://cdn-icons-png.flaticon.com/512/197/197561.png'
-                    className='flag-img'
-                    alt='Philippines Flag'
-                  />
-                </li>
-            </ul>
+            </li>
+            <li>
+              <span>Portugal</span>
+              <img
+                src='https://cdn-icons-png.flaticon.com/512/3909/3909361.png'
+                className='flag-img'
+                alt='Portugal Flag'
+              />
+            </li>
+            <li>
+              <span>Philippines</span>
+              <img
+                src='https://cdn-icons-png.flaticon.com/512/197/197561.png'
+                className='flag-img'
+                alt='Philippines Flag'
+              />
+            </li>
+          </ul>
+          <h2 className='title' id='Introduction'>
+           School</h2>
+          <p className='school'>
+            I graduated{' '}
+            <span id='school'>
+              California State University, Long Beach
+              <img
+                src='https://digitalskills.cpace.csulb.edu/wp-content/uploads/cropped-cropped-favicon.png'
+                loading='lazy'
+                className='img'
+                alt='CSULB Logo'
+              />
+            </span>{' '}
+            with a{' '}
+            <span id='ME'>
+              <br />B.S. in Mechanical Engineering
+              <img
+                src='https://static.vecteezy.com/system/resources/thumbnails/016/415/298/small/wrench-illustration-in-minimal-style-png.png'
+                loading='lazy'
+                className='img'
+                alt='Mechanical Engineering Icon'
+              />
+            </span>{' '}
+            and a{' '}
+            <span id='ME'>
+              B.A. in Physics
+              <img
+                src='https://icons.veryicon.com/png/o/education-technology/educational-icon/physics-4.png'
+                loading='lazy'
+                className='img'
+                alt='Physics Icon'
+              />
+            </span>
+            <br />I also completed{' '}
+            <span>
+              Fullstack Academy{' '}
+              <img
+                src='https://s3.us-east-1.amazonaws.com/fsa2-assets/assets/Grace-Hopper/GH-Full-Lockup-Chalk.png'
+                loading='lazy'
+                className='gh-img'
+                alt='Fullstack Academy Icon'
+              />
+            </span>{' '}
+            for a certification in{' '}
+            <span id='SWE'>
+              <br />Software Development{' '}
+              <img
+                src='https://cdn-icons-png.flaticon.com/512/8759/8759045.png'
+                loading='lazy'
+                className='img'
+                alt='Software Development Icon'
+              />
+            </span>
+            <h2 className='title' id='Introduction'>
+           Skills</h2>
+            <span id='skills'>
+              where I learned ...
+              <Skills />
+              I also know ...
+              <SchoolSkills/>
+            </span>
           </p>
         </div>
       </div>
       <Footer />
-      <Contact/>
+      < Contact/>
     </div>
   );
 };
