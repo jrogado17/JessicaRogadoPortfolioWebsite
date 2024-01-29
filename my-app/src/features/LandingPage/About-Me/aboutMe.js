d0f6af9d8e48958468416d8b9641c58344e76f96
Jessica Rogado <j.rogado17@yahoo.com>
2024-01-29 11:21:42 -0800

    redesigning about me page

--- my-app/src/features/LandingPage/About-Me/aboutMe.js
+++ my-app/src/features/LandingPage/About-Me/aboutMe.js
@@ -2,28 +2,100 @@ import React from 'react';
 import './aboutMe.css';
 import Skills from '../Skills/SWskills';
 import SchoolSkills from '../Skills/Schoolskills';
-import Footer from '../../Footer/footer'
-import Contact from '../Contact/contact'
+import Footer from '../../Footer/footer';
+import Contact from '../Contact/contact';
 
 const AboutMe = () => {
   return (
     <div className='aboutmeContainer'>
-       <h2 className='PageTitle' id='Introduction'>
-            About Me
-          </h2>
-          <div className='introduction-section'>
+      <h2 className='PageTitle' id='Introduction'>
+        About Me
+      </h2>
+
+      <div className='introduction-section'>
         <h2 className='title' id='Introduction'>
           Introduction
         </h2>
         <p className='introduction-text'>
-          Hey there! 👋 I'm Jessica Rogado, a 💡 versatile engineer with a background in Mechanical Engineering and Physics that empowers me to tackle complex challenges, turning them into innovative solutions. 🚀 After my journey at Rivian 
-          <img src='https://static.thenounproject.com/png/5383586-200.png' id='rivian-car' alt='Rivian Car' />, 
-          I seamlessly integrated into the world of software, seizing the opportunity to earn a certification in software development. 💻✨
+          Hey there! 👋 I'm Jessica Rogado, a 💡 versatile engineer with a background in Mechanical Engineering and Physics that empowers me to tackle complex challenges, turning them into innovative solutions. 🚀 After my journey at Rivian{' '}
+          <img src='https://static.thenounproject.com/png/5383586-200.png' id='rivian-car' alt='Rivian Car' />, I seamlessly integrated into the world of software, seizing the opportunity to earn a certification in software development. 💻✨
+        </p>
+      </div>
+      <p id='text-spike-aboutme'>✹</p>
+      <div className='school-section'>
+        <h2 className='title' id='Introduction'>
+          School
+        </h2>
+        <p className='school-text'>
+            I graduated{' '}
+            <span id='school'>
+              California State University, Long Beach
+              <img
+                src='https://digitalskills.cpace.csulb.edu/wp-content/uploads/cropped-cropped-favicon.png'
+                loading='lazy'
+                className='img'
+                alt='CSULB Logo'
+              />
+            </span>{' '}
+            with a{' '}
+            <span id='ME'>
+              <br />B.S. in Mechanical Engineering
+              <img
+                src='https://static.vecteezy.com/system/resources/thumbnails/016/415/298/small/wrench-illustration-in-minimal-style-png.png'
+                loading='lazy'
+                className='img'
+                alt='Mechanical Engineering Icon'
+              />
+            </span>{' '}
+            and a{' '}
+            <span id='ME'>
+              B.A. in Physics
+              <img
+                src='https://icons.veryicon.com/png/o/education-technology/educational-icon/physics-4.png'
+                loading='lazy'
+                className='img'
+                alt='Physics Icon'
+              />
+            </span>
+            <br />I also completed{' '}
+            <span>
+              Fullstack Academy{' '}
+              <img
+                src='https://s3.us-east-1.amazonaws.com/fsa2-assets/assets/Grace-Hopper/GH-Full-Lockup-Chalk.png'
+                loading='lazy'
+                className='gh-img'
+                alt='Fullstack Academy Icon'
+              />
+            </span>{' '}
+            for a certification in{' '}
+            <span id='SWE'>
+              Software Development{' '}
+              <img
+                src='https://cdn-icons-png.flaticon.com/512/8759/8759045.png'
+                loading='lazy'
+                className='img'
+                alt='Software Development Icon'
+              />
+            </span>
+        </p>
+      </div>
+      <div className='skills-section'>
+        <h2 className='title' id='Introduction'>
+          Skills
+        </h2>
+        <p id='skills'>
+          where I learned ...
+          {/* <Skills /> */}
+          I also know ...
+          <SchoolSkills/>
         </p>
       </div>
       <div className='interestsContainer'>
         <div className='interests-text'>
-            <p className='interests'>
+        <h2 className='title' id='Introduction'>
+          Interests
+        </h2>
+          <p className='interests'>
             I love to surf
             <img
               src='https://cdn-icons-png.flaticon.com/512/3145/3145024.png' 
@@ -45,6 +117,7 @@ const AboutMe = () => {
               />
             Where I've been...
           </p>
+          <ul className='country-list'>
           <ul className='country-list'>
             <li>
               <span>Netherlands</span>
@@ -111,72 +184,11 @@ const AboutMe = () => {
               />
             </li>
           </ul>
-          <h2 className='title' id='Introduction'>
-           School</h2>
-          <p className='school'>
-            I graduated{' '}
-            <span id='school'>
-              California State University, Long Beach
-              <img
-                src='https://digitalskills.cpace.csulb.edu/wp-content/uploads/cropped-cropped-favicon.png'
-                loading='lazy'
-                className='img'
-                alt='CSULB Logo'
-              />
-            </span>{' '}
-            with a{' '}
-            <span id='ME'>
-              <br />B.S. in Mechanical Engineering
-              <img
-                src='https://static.vecteezy.com/system/resources/thumbnails/016/415/298/small/wrench-illustration-in-minimal-style-png.png'
-                loading='lazy'
-                className='img'
-                alt='Mechanical Engineering Icon'
-              />
-            </span>{' '}
-            and a{' '}
-            <span id='ME'>
-              B.A. in Physics
-              <img
-                src='https://icons.veryicon.com/png/o/education-technology/educational-icon/physics-4.png'
-                loading='lazy'
-                className='img'
-                alt='Physics Icon'
-              />
-            </span>
-            <br />I also completed{' '}
-            <span>
-              Fullstack Academy{' '}
-              <img
-                src='https://s3.us-east-1.amazonaws.com/fsa2-assets/assets/Grace-Hopper/GH-Full-Lockup-Chalk.png'
-                loading='lazy'
-                className='gh-img'
-                alt='Fullstack Academy Icon'
-              />
-            </span>{' '}
-            for a certification in{' '}
-            <span id='SWE'>
-              <br />Software Development{' '}
-              <img
-                src='https://cdn-icons-png.flaticon.com/512/8759/8759045.png'
-                loading='lazy'
-                className='img'
-                alt='Software Development Icon'
-              />
-            </span>
-            <h2 className='title' id='Introduction'>
-           Skills</h2>
-            <span id='skills'>
-              where I learned ...
-              <Skills />
-              I also know ...
-              <SchoolSkills/>
-            </span>
-          </p>
+          </ul>
         </div>
       </div>
       <Footer />
-      < Contact/>
+      <Contact/>
     </div>
   );
 };
