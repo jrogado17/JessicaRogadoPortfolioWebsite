e88b124b1c85ba01ca32c34cadd72b2cef300a1a
Jessica Rogado <j.rogado17@yahoo.com>
2024-01-31 19:48:05 -0800

    added redesigns to title in about me

---
+++ Archive/Education/education.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import './education.css'
+// import ghfsaImage from '../';
+// import csulbImage from '../../images/csulb.png';
+// import header from '../../images/educationHeader.png'
+
+// const Education = () => {
+//   return (
+//     <div className = "education">
+//         <div className = 'educationHeader'>
+//             <img src={header} alt="Education Header" />
+//         </div>
+//         <div className = 'schools'>
+//             <div className="education-item">
+//                 <img src={ghfsaImage} alt="GHFSA Logo" />
+//                 <p>Certification of Completion</p>
+//             </div>
+//             <div className="education-item">
+//                 <img src={csulbImage} alt="CSULB Logo" />
+//                 <p>B.S. in Mechanical Engineering<br></br> B.A. in Physics</p>
+//             </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Education;
