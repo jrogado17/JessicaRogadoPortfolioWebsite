64f7d9ef0f3b5570101ffe34f6af1e1ebbcc8485
Jessica Rogado <j.rogado17@yahoo.com>
2024-02-05 12:35:35 -0800

    added basename of website to index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App/App';
 
 createRoot(document.getElementById('root')).render(
-  <Router>
+  <Router basename="/JessicaRogadoPortfolioWebsite">
     <App />
   </Router>
 );
