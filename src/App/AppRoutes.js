6bc2b8da55fa8cea0021ac2c2906b628c76ff30e
Jessica Rogado <j.rogado17@yahoo.com>
2024-02-05 21:30:42 -0800

    added hashrouter to replace browserrouter

--- src/App/AppRoutes.js
+++ src/App/AppRoutes.js
@@ -3,15 +3,16 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Welcome from '../features/Pages/Welcome/welcome';
 import AboutMe from "../features/Pages/About-Me/aboutMe";
 import Experience from "../features/Pages/Work-Experiences/workexperiences"; 
+import { HashRouter } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
     <div className="appRoutesContainer">
-      <BrowserRouter basename="/JessicaRogadoPortfolioWebsite">
+      <HashRouter basename="/JessicaRogadoPortfolioWebsite">
         <Route path="/" exact component={Welcome} />
         <Route path="/about" component={AboutMe} />
         <Route path="/experience" component={Experience} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
