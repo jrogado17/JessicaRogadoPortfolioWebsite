258476fc767a89aa06a30d9ac053600fe3576f82
Jessica Rogado <j.rogado17@yahoo.com>
2024-02-05 21:21:17 -0800

    added BrowserRouter in AppRoutes

--- src/App/AppRoutes.js
+++ src/App/AppRoutes.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Welcome from '../features/Pages/Welcome/welcome'
+import { BrowserRouter, Route } from "react-router-dom";
+import Welcome from '../features/Pages/Welcome/welcome';
 import AboutMe from "../features/Pages/About-Me/aboutMe";
 import Experience from "../features/Pages/Work-Experiences/workexperiences"; 
 
 const AppRoutes = () => {
   return (
     <div className="appRoutesContainer">
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
+      <BrowserRouter>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/about" component={AboutMe} />
+        <Route path="/experience" component={Experience} />
+      </BrowserRouter>
     </div>
   );
 };
