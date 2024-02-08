7d5a71bf21f0a7525d08cf0dd4b141718d2ab4c5
Jessica Rogado <j.rogado17@yahoo.com>
2024-02-08 12:36:29 -0800

    added basename to approuter and index.js

--- src/App/AppRoutes.js
+++ src/App/AppRoutes.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Welcome from '../features/Pages/Welcome/welcome'
+import Welcome from '../features/Pages/Welcome/welcome';
 import AboutMe from "../features/Pages/About-Me/aboutMe";
 import Experience from "../features/Pages/Work-Experiences/workexperiences";
-
 
 const AppRoutes = () => {
   return (
     <div className="appRoutesContainer">
-      <Routes>
+      <Routes basename="/JessicaRogadoPortfolioWebsite">
         <Route path="/" element={<Welcome />} />
         <Route path="/about" element={<AboutMe />} />
         <Route path="/experience" element={<Experience />} />
-
       </Routes>
     </div>
   );
